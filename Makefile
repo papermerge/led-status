62ace3580a6e7483019fd1f8a721f8a2e7cd4a49
Eugen Ciur <eugen@papermerge.com>
2021-04-06 07:16:04 +0200

    this is how tests will look like!

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 test:
 	TESTBUILD=true npx webpack
-	npx mocha test-dist/tests.bundle.js --require source-map-support/register
 
 clean:
 	rm -fr test-dist/
--- tests/basic_test.js
+++ tests/basic_test.js
@@ -1,17 +1,42 @@
 import { assert } from "chai";
 
 import _ from "underscore";
+import $ from "jquery";
 import Backbone from "backbone";
 
 import { LEDDocumentStatus } from '../src/js/led_status.js';
 
-
 describe("Basic test suite", function() {
-  it("can instanciate LEDDocumentStatus", function() {
-    let dispatcher = _.clone(Backbone.Events);;
 
+  it("Can instanciate LEDDocumentStatus", function() {
+
+    let dispatcher = _.clone(Backbone.Events);
     let doc_status = new LEDDocumentStatus(dispatcher, {'use_sockets': false});
 
     assert.isDefined(doc_status);
+
   });
+
+  it("Changes status from unkown to in-progress", function() {
+    let dispatcher = _.clone(Backbone.Events);
+
+    let doc_status = new LEDDocumentStatus(dispatcher, {'use_sockets': false});
+
+    assert.equal(
+        $(".led > .led-unknown").length, 1,
+        "Test should start with initial state led-unknown"
+    );
+    dispatcher.trigger(
+        "leds.document", {"document_id": 1, "type": "ocrdocument.started"}
+    );
+    assert.equal(
+        $(".led > .led-unknown").length, 0,
+        "led-unknown was replaced"
+    );
+    assert.equal(
+        $(".led > .led-in-progress").length, 1,
+        "Document ID=1 led now indicate status -in progress-"
+    );
+  });
+
 });
